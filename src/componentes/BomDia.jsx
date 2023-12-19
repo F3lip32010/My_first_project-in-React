4ae3bde1e60b1f8fa23dc70be3dd304ea6bd978d
Felipe <felipemeirelessantana@gmail.com>
2023-12-19 14:12:13 -0300

    c3 - fixes

--- src/componentes/BomDia.jsx
+++ src/componentes/BomDia.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default props => [
     <h1 key="h1">Bom dia {props.nome}!</h1>,
     <h2 key="h2">Até breve!</h2>
